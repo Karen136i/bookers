5180ab0678259cee6f403640dfc538377df7f57a
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-02-11 11:27:53 +0000

    3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new 
+    @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    redirect_to '/index'
   end
 
   def show
@@ -20,16 +21,22 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
-  end 
-  
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
